27343bf1db9a519f34129a45038cf756dc2bfa86
unknown <andrew_tjew@yahoo.com>
2024-05-11 13:44:59 -0400

    currency

--- components/src/main/java/org/nova/localization/CurrencyCode.java
+++ components/src/main/java/org/nova/localization/CurrencyCode.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 
 public enum CurrencyCode
 {
-    CAD(new Currency_ISO_4217("Canadian dollar","CAD","124",2,"$")),
-    USD(new Currency_ISO_4217("United States dollar","USD","840",2,"$")),
-    MYR(new Currency_ISO_4217("Malaysian ringgit","MYR","458",2,"RM")),
-    EUR(new Currency_ISO_4217("Euro","EUR","978",2,"€")),
-    GBP(new Currency_ISO_4217("Pound sterling","GBP","826",2,"£")),
-    CNY(new Currency_ISO_4217("Renminbi","GBP","156",2,"¥")),
-    SGD(new Currency_ISO_4217("Singapore dollar","SGD","702",2,"$")),
+    CAD(new Currency_ISO_4217("Canadian dollar","CAD",124,2,"$")),
+    USD(new Currency_ISO_4217("United States dollar","USD",840,2,"$")),
+    MYR(new Currency_ISO_4217("Malaysian ringgit","MYR",458,2,"RM")),
+    EUR(new Currency_ISO_4217("Euro","EUR",978,2,"€")),
+    GBP(new Currency_ISO_4217("Pound sterling","GBP",826,2,"£")),
+    CNY(new Currency_ISO_4217("Renminbi","GBP",156,2,"¥")),
+    SGD(new Currency_ISO_4217("Singapore dollar","SGD",702,2,"$")),
     ;
     
     private Currency_ISO_4217 value;
--- components/src/main/java/org/nova/localization/Currency_ISO_4217.java
+++ components/src/main/java/org/nova/localization/Currency_ISO_4217.java
@@ -4,11 +4,11 @@ public class Currency_ISO_4217
 {
     final public String name;
     final public String alphabeticCode;
-    final public String numericCode;
+    final public int numericCode;
     final public int digits;
     final public String symbol;
     
-    public Currency_ISO_4217(String name,String alphabeticCode,String numericCode,int digits,String symbol)
+    public Currency_ISO_4217(String name,String alphabeticCode,int numericCode,int digits,String symbol)
     {
         this.name=name;
         this.alphabeticCode=alphabeticCode;
