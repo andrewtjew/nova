6b5a465a7021d7dcec9e472a0f4f7a48dbe26b54
unknown <andrew_tjew@yahoo.com>
2024-06-26 11:06:03 -0400

    sync

--- bootstrap/bootstrap5.2.0/src/main/java/org/nova/html/bootstrap/InputGroup.java
+++ bootstrap/bootstrap5.2.0/src/main/java/org/nova/html/bootstrap/InputGroup.java
@@ -29,40 +29,6 @@ public class InputGroup extends StyleComponent<InputGroup>
         super("div","input-group");
     }
 
-//    public InputGroup prepend(Element element)
-//    {
-////        returnAddInner(new InputGroupPrepend()).addInner(new InputGroupText().addInner(element));
-//        returnAddInner(new InputGroupPrepend()).addInner(element);
-//        return this;
-//    }
-//
-//    public InputGroup append(Element element)
-//    {
-//        returnAddInner(new InputGroupAppend()).addInner(element);
-////        returnAddInner(new InputGroupAppend()).addInner(new InputGroupText().addInner(element));
-//        return this;
-//    }
-//
-//    public InputGroup prepend(String text)
-//    {
-//        returnAddInner(new InputGroupPrepend()).returnAddInner(new InputGroupText(text));
-//        return this;
-//    }
-//
-//    public InputGroup prepend(String text,int percentageWidth)
-//    {
-//        InputGroupPrepend prepend=returnAddInner(new InputGroupPrepend()).style("flex: 0 0 "+percentageWidth+"%;");
-//        
-//        InputGroupText groupText=prepend.returnAddInner(new InputGroupText(text));
-//        Styler.style(groupText).w(100);
-//        return this;
-//    }
-//
-//    public InputGroup append(String text)
-//    {
-//        returnAddInner(new InputGroupAppend()).returnAddInner(new InputGroupText(text));
-//        return this;
-//    }
 
     
 }
