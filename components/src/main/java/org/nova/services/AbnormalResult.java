32cdb35e944d6c65e5018a5c798ca97e02fbc0f1
Andrew Tjew <atjew@evolvesoftworks.com>
2026-03-10 20:51:49 +0000

    sync

--- components/src/main/java/org/nova/services/AbnormalResult.java
+++ components/src/main/java/org/nova/services/AbnormalResult.java
@@ -1,6 +1,5 @@
 package org.nova.services;
 
-import org.nova.html.elements.Element;
 import org.nova.http.server.Response;
 
 public record AbnormalResult<CONTENT>(String seeOther,Integer statusCode,Response<CONTENT> response)
