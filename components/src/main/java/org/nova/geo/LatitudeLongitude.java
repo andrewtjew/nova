313a10f56a8b0520f7cccfe16635601482d7a713
unknown <andrew_tjew@yahoo.com>
2026-03-19 23:53:37 -0400

    save

---
+++ components/src/main/java/org/nova/geo/LatitudeLongitude.java
@@ -0,0 +1,16 @@
+package org.nova.geo;
+
+public class LatitudeLongitude
+{
+    public double latitude;
+    public double longitude;
+    public LatitudeLongitude(double latitude,double longitude)
+    {
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+    public LatitudeLongitude()
+    {
+    }
+    
+}
